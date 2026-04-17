23726234e8eb4b3df60b2a5077400ca0d64ad7c7
Raja68-ui <rajamadhurakavi@gmail.com>
2026-04-18 01:35:31 +0530

    Dynamically create uploads dictionary in production environment

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -43,6 +43,12 @@ router.get('/:id', async (req, res) => {
 
 const path = require('path');
 const multer = require('multer');
+const fs = require('fs');
+
+const uploadDir = 'uploads/';
+if (!fs.existsSync(uploadDir)){
+    fs.mkdirSync(uploadDir, { recursive: true });
+}
 
 const storage = multer.diskStorage({
   destination: function(req, file, cb) {
