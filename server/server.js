490e83a8813c06b5a440269f4fd293b584fe46fe
Raja68-ui <rajamadhurakavi@gmail.com>
2026-04-18 00:33:20 +0530

    Fix CORS origin bypass to allow Vercel dynamic domains

--- server/server.js
+++ server/server.js
@@ -7,7 +7,9 @@ dotenv.config();
 
 const app = express();
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: function (origin, callback) {
+    callback(null, true);
+  },
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true
